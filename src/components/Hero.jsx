2d422233cbbb77174746931822911289cd496435
theCodeCrusaderX <bobi726544@gmail.com>
2024-12-12 17:21:48 +0530

    hero section completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,120 @@
+import React, { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+function Hero() {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(1);
+
+  const totalVideos = 4;
+
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoaded = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const upcommingIndex = (currentIndex % totalVideos) +1
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+    
+    setCurrentIndex(upcommingIndex);
+
+  };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  const getVideoSource = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden ">
+      <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+        <div>
+          <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg ">
+
+            {/* miniVideo scale animation done under this div  */}
+            <div
+              onClick={handleMiniVideoClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSource(upcommingIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoaded}
+              >
+                
+              </video>
+            </div>
+          </div>
+
+          
+          <video
+          ref={nextVideoRef} 
+          src={getVideoSource(currentIndex)}
+          loop
+          muted
+          id="next-video"
+          className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          onLoadedData={handleVideoLoaded}
+          />
+
+          <video 
+            src={getVideoSource(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoaded}
+          />
+
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 z-40 right-5 text-blue-75">G<b>a</b>ming</h1>
+        <div className="absolute top-0 left-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">redifi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br /> Unleash the Plat Economy</p>
+            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="bg-yellow-300 flex-center gap-1" />
+          </div>
+        </div>
+      </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">G<b>a</b>ming</h1>
+    </div>
+  );
+}
+
+export default Hero;
